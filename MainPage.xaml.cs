8d3ce6104ca1bd7985aa9cffa3a31c3eee391836
ana-bulatovic <abulatovic5@gmail.com>
2023-04-13 13:41:13 +0200

    added dependency injection

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,21 @@
-﻿using WordleClone.ViewModel;
+﻿using WordleClone.Services;
+using WordleClone.ViewModel;
 
 namespace WordleClone;
 
 public partial class MainPage : ContentPage
 {
-
+	private GameViewModel _gameViewModel;
 	public MainPage(GameViewModel viewModel)
 	{
 		InitializeComponent();
 		BindingContext = viewModel;
+		_gameViewModel = viewModel;
 	}
 
-
-
+	//protected override void OnAppearing()
+	//{
+ //       _gameViewModel.GetSolution();
+ //   }
 }
 
