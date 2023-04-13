345c52295199da3c1c68b9f296a125fa18e17ee2
ana-bulatovic <abulatovic5@gmail.com>
2023-04-13 13:39:03 +0200

    added class which will store solution definition

---
+++ Model/WordEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleClone.Model
+{
+    public class WordEntry
+    {
+        public string PartOfSpeech { get; set; }
+        public string Definition { get; set; }
+
+        public WordEntry(string partOfSpeech, string definition)
+        {
+            PartOfSpeech = partOfSpeech;
+            Definition = definition;
+        }
+
+    }
+
+}
